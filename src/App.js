1b404f5bac131490c95f6c431d7aec0fb7f631de
natasha.prabhoo@gmail.com <67606518+natashamp@users.noreply.github.com>
2024-04-24 20:02:46 -0700

    upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
+import React, { useState } from 'react'; // Import the useState hook from the react package
 import './App.css';
+import { Login } from "./Login"; // Import the Login component
+import { Register } from "./Register"; // Import the Register component
+import { LandingPage } from "./LandingPage"; // Import the LandingPage component
+import { Questionare } from "./Questionare"; // Import the LandingPage component
+import { CreateTeam } from './CreateTeam'; // Import the CreateTeam component
+import { BrowserRouter as Router, Routes, Route, Switch } from 'react-router-dom'; // Import the BrowserRouter, Route, and Switch components from the react-router-dom package
 
 function App() {
+  const [currentForm, setCurrentForm] = useState('login');
+
+  const toggleForm = (formName) => {
+    setCurrentForm(formName);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router> 
+        <Routes>
+          <Route path="/" element={<Login/>} />
+          <Route path="/register" element={<Register/>} />
+          <Route path="/landingpage" element={<LandingPage/>} />
+          <Route path="/questionare" element={<Questionare/>} />
+          <Route path="/createteam" element={<CreateTeam/>} />
+          <Route path="*" element={<h1>404 Not Found</h1>} />
+        </Routes>
+      </Router>
+
+      
     </div>
   );
 }
+/*
+  {
+        currentForm === "login" && <Login onFormSwitch={toggleForm} />
+      }
+      {
+        currentForm === "register" && <Register onFormSwitch={toggleForm} />
+      }
+      {
+        currentForm === "landing" && <LandingPage onFormSwitch={toggleForm} />
+      }
+      <Route path="/register" element={<Register onFormSwitch={toggleForm} />} />
+          <Route path="/landingpage" element={<LandingPage onFormSwitch={toggleForm} />} />
+        </Switch>
+*/
 
 export default App;
